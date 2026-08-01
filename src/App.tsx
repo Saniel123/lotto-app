305c59719d75ba9aa65e0cca4dff968f00acce45
MyApplicationSaniel <myrtledwainsaniel@gmail.com>
2026-08-01 10:22:28 +0800

    lotto-app first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+
+  return (
+    <> 
+    <h1 className='text-center fs-5xl'>Lotto App</h1>
+<Button>Submit</Button>
+    </>
+  )
+}
+
+export default App
